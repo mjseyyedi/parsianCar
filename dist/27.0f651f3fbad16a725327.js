74e723a4ae2f0380c24b5f4c3469d106a81447ff
Mohammad Javad <mohamadjavad.seyyedi@zoodfood.com>
2020-04-06 22:39:35 +0430

    commit built files

---
+++ dist/27.0f651f3fbad16a725327.js
@@ -0,0 +1,1 @@
+(window.__LOADABLE_LOADED_CHUNKS__=window.__LOADABLE_LOADED_CHUNKS__||[]).push([[27],{294:function(_,n,t){"use strict";t.r(n),t.d(n,"fetching",(function(){return i}));var i=function(_,n){return[]}}}]);
